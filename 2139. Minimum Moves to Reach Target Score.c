deed9f46b86127ac7c81fdab078508ec04c6d1fa
ignacio <111444434+ignaciofre@users.noreply.github.com>
2022-09-02 15:10:59 +0400

    minimum moves recursion

---
+++ 2139. Minimum Moves to Reach Target Score.c
@@ -0,0 +1,17 @@
+/*
+You are playing a game with integers. You start with the integer 1 and you want to reach the integer target. In one move, you can either increment or double the current integer. You can use the increment operation any number of times, however, you can only use the double operation at most maxDoubles times. Given the two integers target and maxDoubles, return the minimum number of moves needed to reach target starting with 1.
+*/
+
+int minMoves(int target, int maxDoubles) {
+    if (target <= 1) {
+        return 0;
+    }
+    else {
+        if ((maxDoubles >= 1) && (target/2 >= 1)) {
+            return 1 + minMoves(target/2, maxDoubles-1);
+        }
+        else {
+            return 1 + minMoves(target-1, maxDoubles);
+        }
+    }
+}
